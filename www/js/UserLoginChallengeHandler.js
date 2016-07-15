059395673765067edecb1b3717a29e2db5ec6ef2
Idan Adar <iadar@il.ibm.com>
2016-07-15 08:52:58 +0300

    Support Windows. Use navigator.notification.alert
    Signed-off-by: Idan Adar <iadar@il.ibm.com>

--- www/js/UserLoginChallengeHandler.js
+++ www/js/UserLoginChallengeHandler.js
@@ -47,9 +47,9 @@ var UserLoginChallengeHandler = function() {
         WL.Logger.debug("handleFailure: " + error.failure);
         isChallenged = false;
         if (error.failure !== null){
-            alert(error.failure);
+            navigator.notification.alert(error.failure);
         } else {
-            alert("Failed to login.");
+            navigator.notification.alert("Failed to login.");
         }
     };
 
@@ -57,7 +57,7 @@ var UserLoginChallengeHandler = function() {
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         if (username === "" || password === ""){
-            alert("Username and password are required");
+            navigator.notification.alert("Username and password are required");
             return;
         }
         if (isChallenged){
